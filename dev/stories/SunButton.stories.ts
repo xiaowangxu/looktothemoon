05f161a646873866c209e0747d23f6ce4239aca8
leixingmeng <948495875@qq.com>
2024-02-22 10:05:12 +0800

    disabled pointer event

--- dev/stories/SunButton.stories.ts
+++ dev/stories/SunButton.stories.ts
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/vue3';
 import SunButton from '../../src/sundesign/button/SunButton.vue';
 import { Args, ArgsTypes, Decorators } from './SunDesignArgs';
 import { Search, X } from 'lucide-vue-next';
+import SunIcon from '../../src/sundesign/icon/SunIcon.vue';
 
 const meta: Meta<typeof SunButton> = {
     component: SunButton,
@@ -20,7 +21,7 @@ export const Button: Story = {
     decorators: Decorators,
     tags: ['autodocs'],
     render: (args) => ({
-        components: { SunButton, Search, X },
+        components: { SunButton, Search, X, SunIcon },
         setup() {
             return { args };
         },
@@ -29,6 +30,7 @@ export const Button: Story = {
 			  <SunButton v-bind="args"><Search />按钮</SunButton>
 			  <SunButton v-bind="args" squared><Search /></SunButton>
 			  <SunButton v-bind="args" disabled>disabled</SunButton>
+			  <SunButton v-bind="args"><SunIcon name="Loader2" rotating/>按钮</SunButton>
 			  <div style="display: flex; flex-wrap: nowrap;">
 			  	  <SunButton v-bind="args" :border-mask="0b1001" style="margin-right: -1px;">查找</SunButton>
 			  	  <SunButton v-bind="args" :border-mask="0b0110" squared><X /></SunButton>
@@ -46,14 +48,14 @@ export const Button: Story = {
 export const IconOnly: Story = {
     decorators: Decorators,
     render: (args) => ({
-        components: { SunButton, Search },
+        components: { SunButton, Search, SunIcon },
         setup() {
             return { args };
         },
         template: `
-			<SunButton v-bind="args" size="small" squared><Search /></SunButton>
-			<SunButton v-bind="args" size="normal" squared><Search /></SunButton>
-			<SunButton v-bind="args" size="large" squared><Search /></SunButton>
+			<SunButton v-bind="args" size="small" squared><SunIcon name="Loader" rotating /></SunButton>
+			<SunButton v-bind="args" size="normal" squared><SunIcon name="Loader" rotating /></SunButton>
+			<SunButton v-bind="args" size="large" squared><SunIcon name="Loader" rotating /></SunButton>
 		`,
     }),
     argTypes: {
--- src/sundesign/icon/SunIcon.vue
+++ src/sundesign/icon/SunIcon.vue
@@ -1,7 +1,8 @@
 <template>
-    <component v-if="icon !== undefined" class="__sun-design__ __sun-design-icon__" :is="icon" :color="color" />
+    <component v-if="icon !== undefined" class="__sun-design__ __sun-design-icon__" :is="icon" :color="color"
+        :class="{ 'animate-rotate': rotating }" />
     <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-        class="__sun-design__ __sun-design-icon__">
+        class="__sun-design__ __sun-design-icon__" :class="{ 'animate-rotate': rotating }">
     </svg>
 </template>
 
@@ -14,10 +15,26 @@ import * as icons from "lucide-vue-next";
 const props = defineProps<{
     name?: string,
     color?: string,
+    rotating?: boolean,
 }>();
 
 // data
 const icon = computed(() => props.name === undefined || props.name === '' ? undefined : (icons as any)[props.name] as FunctionalComponent | undefined);
 
 </script>
+
+<style>
+.__sun-design-icon__.animate-rotate {
+    animation: __sun-design-animate-rotate__ 2s linear infinite;
+}
+
+@keyframes __sun-design-animate-rotate__ {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
 
